849bd10d6716163631128e959c8b40d722e8f68c
melissa wiggall <awiggall@aol.co.uk>
2023-07-11 15:34:40 +0100

    14.34 js

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const stringPassIn = `https://api.nasa.gov/planetary/apod?api_key=vVoCVxcepv4Ob0JI2VysjrNxtZ9RGTE6MYlwFjtW`
-let responseData;
 
-fetch (stringPassIn)
-.then((res) => res.json())
-.then((responseData) => {
-    const h1 = responseData.title; //this is the data coming from NASA
-    const h3 = responseData.date;
-    const p = responseData.explanation;
-    const myImg = responseData.url;
 
-    document.querySelector('h1').innerHTML = `NASA Daily Photo ${h1}`;//daily title changes/ is dynamic.
-    document.querySelector('h3').innerHTML = h3; //todays date updates
-    document.querySelector('p').innerHTML = p; //information about the photo
-    document.getElementById('myImg').src = myImg; //dynamic image! 
+fetch (stringPassIn)            //fetch dat from the api
+.then((res) => res.json())      //parses the data from JSON back into useable data
+.then((resData)  => {           //re-names the data with variables i can inject into the dom
+    heading = resData.title;
+    date = resData.date;
+    textBlock = resData.explanation;
+    dailyImg = resData.url;
 
+document.querySelector('h1').innerHTML = `NASA Daily Photo ${heading}`;//first three are dynamic text
+document.querySelector('h3').innerHTML = date;
+document.querySelector('p').innerHTML  = textBlock;
+document.getElementById('myImg').src   = dailyImg; //dynamic image
 
 });
